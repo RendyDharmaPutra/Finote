ab4091744b4181fd874d7c415ebdb5c8abe66cfe
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-06-24 14:09:31 +0700

    remove(visualize): remove visualize feature refactor(dashboard): fixed bug while redirecting to dashboard after login

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\DashboardController;
+use Inertia\Inertia;
+
 
 
 Route::get('/', function () {
     return redirect('dashboard');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    // Route::get('dashboard', function () {
-
-
-    //     return Inertia::render('dashboard');
-    // })->name('dashboard');
-    Route::resource('dashboard', DashboardController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/balance.php';
 require __DIR__.'/income.php';
 require __DIR__.'/outcome.php';
-require __DIR__.'/visualize.php';
 require __DIR__.'/auth.php';
